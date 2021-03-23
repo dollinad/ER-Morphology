694300d8bc35c4d0e8c35f88b4fa980b4635e334
Danilo Lekovic <danilo@lekovic.ca>
2021-03-23 09:36:53 -0700

    Added function that plots all frangi filters of .tiff. Can use this to get a look at all filtered images quickly

--- Filtering/frangi.py
+++ Filtering/frangi.py
@@ -5,22 +5,39 @@ from skimage import io
 import tifffile 
 import matplotlib.pyplot as plt
 
-# tif file is read using 'tifffile' plugin
-im = io.imread("data.tif", plugin='tifffile')
+def example():
+    # tif file is read using 'tifffile' plugin
+    im = io.imread("data.tif", plugin='tifffile')
+    
+    fig, ax = plt.subplots(ncols=2)
+    
+    # Show 'before' image
+    # Index position 9 is a random selection
+    ax[0].imshow(im[9], cmap=plt.cm.gray)
+    ax[0].set_title('Original Image')
+    
+    # Show 'after' image
+    ax[1].imshow(frangi(im[9]), cmap=plt.cm.gray)
+    ax[1].set_title('Frangi Filter on Image\n~')
 
-fig, ax = plt.subplots(ncols=2)
+    for a in ax:
+        a.axis('off')
 
-# Show 'before' image
-# Index position 9 is a random selection
-ax[0].imshow(im[9], cmap=plt.cm.gray)
-ax[0].set_title('Original Image')
+    plt.tight_layout()
+    plt.show()
 
-# Show 'after' image
-ax[1].imshow(frangi(im[9]), cmap=plt.cm.gray)
-ax[1].set_title('Frangi Filter on Image\n~')
+def entireFile():
+    # tif file is read using 'tifffile' plugin
+    im = io.imread("data.tif", plugin='tifffile')
+    fig = plt.figure(figsize=(85, 85))
 
-for a in ax:
-    a.axis('off')
+    for i in range(len(im)):
+        sub = fig.add_subplot(len(im), 10, i + 1)
+        sub.imshow(frangi(im[i]), interpolation='nearest', cmap=plt.cm.gray)
+        sub.set_title('#' + str(i + 1))
+        sub.axis('off')
+    
+    fig.tight_layout()
+    plt.show()
 
-plt.tight_layout()
-plt.show()
+entireFile()
