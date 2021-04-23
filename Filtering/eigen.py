de8b7e27f3a3605a4a441a02653855498e3c7fc8
Cameron <cway140@gmail.com>
2021-04-22 18:27:35 -0700

    Playing with new tube conditions

--- Filtering/eigen.py
+++ Filtering/eigen.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import random
 import matlab.engine
 import os
+import cv2
+import math
 eng = matlab.engine.start_matlab()
 
 
@@ -26,66 +28,52 @@ def getEigs3D(filename, num_slices):
 def highlight_3D(filename):
     im = Image.open(filename)
     width, height = im.size
-    num_slices = 5 #im.n_frames
-    # cv2_im = cv2.imread(filename)
-    # eigvals, frangiImg = getEigs(cv2_im)
-    # print(eigvals[0])
+    num_slices = 2 #im.n_frames
+    # cv2_im = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    # eigVals, frangiImg = getEigs(cv2_im)
 
     # Lambda1 < Lambda2 < Lambda3
-    Lambda1, Lambda2, Lambda3 = getEigs3D(filename, num_slices)
+    Lambda3, Lambda2, Lambda1 = getEigs3D(filename, num_slices)
 
     tube = 0
     sheet = 0
+    highVal = 1E-8
+    lowVal = 1E-10
 
     # convert to numpy arrays
-    Lambda1 = np.array(Lambda1._data).reshape((width, height, num_slices))
-    Lambda2 = np.array(Lambda2._data).reshape((width, height, num_slices))
-    Lambda3 = np.array(Lambda3._data).reshape((width, height, num_slices))  
+    Lambda1 = np.array(Lambda1._data).reshape((num_slices, width, height))
+    Lambda2 = np.array(Lambda2._data).reshape((num_slices, width, height))
+    Lambda3 = np.array(Lambda3._data).reshape((num_slices, width, height))
     
     tempIm = im
     images = []
-    for z in range(num_slices):
+
+    for z in range(1):
         im = tempIm
         im.seek(z)
         im.mode = 'I'
         im = im.point(lambda i:i*(1./256)).convert('RGB')
-        images.append(im)
-
-    for z in range(num_slices):
-        im = images[z]
         for x in range(width):
             for y in range(height):
                 # L1 < L2 < L3
-                L3 = Lambda3[y, x, z]
-                L2 = Lambda2[y, x, z]
-                L1 = Lambda1[y, x, z]
-                
-                if(x == 1000 and y == 1000 and z == 0):
-                    print(L3)
-                    print(L2)
-                    print(L1)
-                highVal = 1E-8
-                lowVal = 1E-12
+                # sort eigenvalues
+                lambdas = [Lambda1[z, y, x], Lambda2[z, y, x], Lambda3[z, y, x]]
+                lambdas.sort
+                if(x == 671 and y == 942):
+                    print(lambdas)
+                L3 = lambdas[0]
+                L2 = lambdas[1]
+                L1 = lambdas[2]
 
                 #check tube / sheet conditions
-                HHL = (L3 >= highVal) and (L2 >= highVal) and (abs(L1) <= lowVal)
-                HHL2 = (L3 <= -highVal) and (L2 <= -highVal) and (abs(L1) <= lowVal)
 
-                HLL = (L3 >= highVal) and (L2 < lowVal or L2 > -lowVal) and (abs(L1) <= lowVal)
-                HLL2 = (L3 <= -highVal) and (L2 < lowVal or L2 > -lowVal) and (abs(L1) <= lowVal)
-
-                is_tube = HHL or HHL2
-                is_sheet = HLL or HLL2
-
-                if is_tube:
+                if abs(L3) > abs(L1)*2 and abs(L2) > abs(L1)*2 and L1*L2 > 0 and  im.getpixel((y,x)) != (0, 0, 0):
                     im.putpixel((y, x), (255, 0, 0)) # tube
                     tube += 1
-                elif is_sheet:
-                    im.putpixel((y, x), (0, 255, 0)) # sheet
-                    sheet += 1
-        images[z] = im
-    
+        images.append(im)
+
     im = images[0]
+
 
     plt.imshow(im)
     plt.title('Eigen highlighting')
@@ -96,82 +84,61 @@ def highlight_3D(filename):
 
 def highlight(fileName, page):
     im = Image.open(fileName)
-
     im.seek(page)
-
+    #im=crop_center(im, 1050, 400)
     tempIm = im.copy()
     tempIm.mode = 'I'
     tempIm = tempIm.point(lambda i:i*(1./256)).convert('RGB')
-
-    eigVals, frangiImg = getEigs(im)    
-    
+    eigVals, frangiImg = getEigs(im)
     im = tempIm
     original = tempIm.copy()
-
-    
+    width, height = im.size
     tube = 0
     sheet = 0
-    
     # Thresholds to change
     highVal = 1E-8
     lowVal = 1E-10
-    
     # L, L => no structure just noise
     # H-, L OR H+, L => sheet like
     # H+, H+ OR H-, H- => tube like
     # 
     # histogram
     # => look at distribution of lambda
-
     # L is very close to zero
     # H- is farther from zero, but negative
     # H+ is closer to zero, but positive
-
-    # final_img = getAvg(original)
-
-    width, height = im.size
-
-    for x in range(width):
-        for y in range(height):
-            # L1 < L2
+    L2_list=[]
+    L3_list=[]
+    for x in range(height):
+        for y in range(width):
             L3 = eigVals[0, x, y]
             L2 = eigVals[1, x, y]
-            
-            highVal = 1E-7
-            lowVal = 1E-12
-
-            # is_tube = abs(L1) < lowVal and abs(L2) > highVal
-            # is_sheet = abs(L1) < lowVal and abs(L2) < lowVal
-            HH = (L3 >= highVal) and (L2 >= highVal)
-            HH2 = (L3 <= -highVal) and (L2 <= -highVal)
-
-            HL = (L3 >= highVal) and (L2 < lowVal or L2 > -lowVal)
-            HL2 = (L3 <= -highVal) and (L2 < lowVal or L2 > -lowVal)
-
-            if HH or HH2:
+            L2_list.append(L2)
+            L3_list.append(L3)
+            # HH = (L3 >= highVal) and (L2 >= highVal)
+            # HH2 = (L3 <= -highVal) and (L2 <= -highVal)
+            # HL = (L3 >= highVal) and (L2 < lowVal or L2 > -lowVal)
+            # HL2 = (L3 <= -highVal) and (L2 < lowVal or L2 > -lowVal)
+            if ((abs(L3) < abs(L2)) and (L3>0)):
                 im.putpixel((y, x), (255, 0, 0)) # tube
                 tube += 1
-            elif HL or HL2:
-                im.putpixel((y, x), (0, 255, 0)) # sheet
-                sheet += 1
-        
 
 
-    fig, ax = plt.subplots(ncols=1)
-    
-    # ax[0].imshow(original)
-    # ax[0].set_title('Original Image')
-    
-    # ax[1].imshow(frangiImg)
-    # ax[1].set_title('Frangi Image')
+    kwargs = dict(alpha=0.5, bins=1000)
+    plt.hist(L2_list, **kwargs, color='g', label='L2')
+    plt.hist(L3_list, **kwargs, color='b', label='L3')
+    plt.show()
 
-    ax[0].imshow(im)
-    ax[0].set_title('Eigen highlighting')
-
+    fig, ax = plt.subplots(ncols=3)
+    ax[0].imshow(original)
+    ax[0].set_title('Original Image')
+    ax[1].imshow(frangiImg)
+    ax[1].set_title('Frangi Image')
+    ax[2].imshow(im)
+    ax[2].set_title('Eigen highlighting')
+    plt.show()
     for a in ax:
         a.axis('off')
-    plt.tight_layout()
-    plt.show()
     return fig
 
 highlight_3D("Best/STED HT1080 siCTRL. Decon_Series008_decon_z00_ch02.tif")
